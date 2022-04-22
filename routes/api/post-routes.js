61cc35804134dc452222508c3219df3156b505fa
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-04-21 18:30:17 -0700

    Pause

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
+
 
 // get all users
 router.get('/', (req, res) => {
@@ -59,6 +61,17 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 
 router.put('/:id', (req, res) => {
   Post.update(

4d85b6d75e2e40414c5f52cb3e97b3c7a531d538
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-04-21 21:54:07 -0700

    Connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
 
-// require('dotenv').config();
+require('dotenv').config();
 
 // create connection to our db
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,
-const sequelize = new Sequelize(,
+const sequelize = new Sequelize(
+  // 'just_tech_news_db', 'root', 'T@ti28Al@n18',
+process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,
   {
   host: 'localhost',
   dialect: 'mysql',

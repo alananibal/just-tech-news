10f675f06c2a604a9d5899de1c3dbe6d4e10a71d
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-05-10 11:06:27 -0700

    bug

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const path = require('path');
 const express = require('express');
+<<<<<<< .merge_file_a09316
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+=======
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const path = require('path');
+>>>>>>> .merge_file_a29680
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -33,6 +39,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
